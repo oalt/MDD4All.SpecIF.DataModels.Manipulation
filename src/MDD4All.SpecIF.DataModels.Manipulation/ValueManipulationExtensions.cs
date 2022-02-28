0e2331804e19205623998e482fac0b00011d25c2
Oliver Alt <oa@lacheweg21.de>
2022-02-28 15:41:47 +0100

    Add extension for duration conversion

---
+++ src/MDD4All.SpecIF.DataModels.Manipulation/ValueManipulationExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Xml;
+
+namespace MDD4All.SpecIF.DataModels.Manipulation
+{
+    public static class ValueManipulationExtensions
+    {
+        public static TimeSpan ToTimeSpan(this string stringValue)
+        {
+            TimeSpan result = default;
+            try
+            {
+                result = XmlConvert.ToTimeSpan(stringValue);
+            }
+            catch(Exception exception) 
+            {
+                ;
+            }
+
+            return result;
+        }
+    }
+}
